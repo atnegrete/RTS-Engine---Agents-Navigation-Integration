a9040286aed4816d27f8dfe71bfc000cdf2fb517
anegrete <alan.t.negrete@gmail.com>
2024-12-24 12:28:52 -0600

    Adding separation avoidance configuration

--- Editor/UnitSetupEditor.cs
+++ Editor/UnitSetupEditor.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.AI;
@@ -7,100 +5,127 @@ using Unity.VisualScripting;
 using ProjectDawn.Navigation.Hybrid;
 using RTSEngine.Movement;
 
-[CustomEditor(typeof(UnitSetup))]
-public class UnitSetupEditor : Editor
-{
-    
-    protected UnitSetup Instance {  get; set; }
-    SerializedProperty ConfigCompleted;
+namespace RTSEngineNavAgentsIntegration {
 
-    SerializedProperty UnitType;
-
-    SerializedProperty AgentsNavController;
-    SerializedProperty Agent;
-    SerializedProperty Shape;
-    SerializedProperty NavMesh;
-    SerializedProperty Avoid;
-    SerializedProperty SmartStop;
-
-    private void OnEnable()
+    [CustomEditor(typeof(UnitSetup))]
+    public class UnitSetupEditor : Editor
     {
-        Instance = (UnitSetup)target;
 
-        ConfigCompleted = serializedObject.FindProperty("ConfigCompleted");
+        protected UnitSetup Instance { get; set; }
+        SerializedProperty ConfigCompleted;
 
-        AgentsNavController = serializedObject.FindProperty("AgentsNavController");
-        UnitType = serializedObject.FindProperty("UnitType");
-        Agent = serializedObject.FindProperty("Agent");
-        Shape = serializedObject.FindProperty("Shape");
-        NavMesh = serializedObject.FindProperty("NavMesh");
-        Avoid = serializedObject.FindProperty("Avoid");
-        SmartStop = serializedObject.FindProperty("SmartStop");
+        SerializedProperty UnitType;
+        SerializedProperty AvoidanceType;
 
-    }
+        SerializedProperty AgentsNavController;
+        SerializedProperty Agent;
+        SerializedProperty Shape;
+        SerializedProperty NavMesh;
+        SerializedProperty Avoid;
+        SerializedProperty Separation;
+        SerializedProperty SmartStop;
 
-    public override void OnInspectorGUI()
-    {
-        serializedObject.Update();
-        string[] mainRow = { "Configuration", "Debug" };
-        Instance.TabID = GUILayout.SelectionGrid(Instance.TabID, mainRow, 2);
-
-        switch (Instance.TabID)
+        private void OnEnable()
         {
-            case 0:
-                EditorGUILayout.BeginVertical("box");
-                EditorGUILayout.PropertyField(ConfigCompleted);
-                EditorGUILayout.PropertyField(UnitType);
-                if(Instance.UnitType != global::UnitType.None)
-                {
-                    if (GUILayout.Button("Configure Unit"))
-                    {
-                        if (Instance.transform.TryGetComponent(out NavMeshAgent OldAgent))
-                        {
-                            DestroyImmediate(OldAgent);
-                        }
+            Instance = (UnitSetup)target;
 
-                        if (Instance.transform.TryGetComponent(out NavMeshObstacle OldObstacle))
-                        {
-                            DestroyImmediate(OldObstacle);
-                        }
+            ConfigCompleted = serializedObject.FindProperty("ConfigCompleted");
 
-                        Instance.AgentsNavController = Instance.AddComponent<AgentsNavController>();
-                        Instance.AgentsNavController.navAgent = Instance.AddComponent<AgentAuthoring>();
-                        Instance.AgentsNavController.agentShape = Instance.AddComponent<AgentCylinderShapeAuthoring>();
-                        Instance.AgentsNavController.agentNavmesh = Instance.AddComponent<AgentNavMeshAuthoring>();
-                        Instance.AgentsNavController.agentAvoidance = Instance.AddComponent<AgentAvoidAuthoring>();
-                        Instance.AddComponent<AgentSmartStopAuthoring>();
-                        switch (Instance.UnitType)
-                        {
-                            case global::UnitType.Infantry:
-                                Instance.AddComponent<InfantryLocomotionAuthoring>();
-                                break;
-                            case global::UnitType.Wheeled:
-                                Instance.AddComponent<WheeledLocomotionAuthoring>();
-                                break;
-                            case global::UnitType.Tracked:
-                                Instance.AddComponent<TrackedLocomotionAuthoring>();
-                                break;
-                            case global::UnitType.Air:
-                                Instance.AddComponent<AircraftLocomotionAuthoring>();
-                                break;
-                        }
+            AgentsNavController = serializedObject.FindProperty("AgentsNavController");
+            UnitType = serializedObject.FindProperty("UnitType");
+            AvoidanceType = serializedObject.FindProperty("AvoidanceType");
+            Agent = serializedObject.FindProperty("Agent");
+            Shape = serializedObject.FindProperty("Shape");
+            NavMesh = serializedObject.FindProperty("NavMesh");
+            Avoid = serializedObject.FindProperty("Avoid");
+            Separation = serializedObject.FindProperty("Separation");
+            SmartStop = serializedObject.FindProperty("SmartStop");
 
-                    }
-                }
-                
-                EditorGUILayout.EndVertical();
-                break;
-            case 1:
-                EditorGUILayout.BeginVertical("box");
-
-
-                EditorGUILayout.EndVertical();
-                break;
         }
 
-        serializedObject.ApplyModifiedProperties();
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            string[] mainRow = { "Configuration", "Debug" };
+            Instance.TabID = GUILayout.SelectionGrid(Instance.TabID, mainRow, 2);
+
+            switch (Instance.TabID)
+            {
+                case 0:
+                    EditorGUILayout.BeginVertical("box");
+                    EditorGUILayout.PropertyField(ConfigCompleted);
+                    EditorGUILayout.PropertyField(UnitType);
+                    EditorGUILayout.PropertyField(AvoidanceType);
+
+                    if (Instance.UnitType != RTSEngineNavAgentsIntegration.UnitType.None)
+                    {
+                        if (GUILayout.Button("Configure Unit"))
+                        {
+                            bool useSonarAvoidance = Instance.AvoidanceType == RTSEngineNavAgentsIntegration.AvoidanceType.Sonar;
+
+                            if (Instance.transform.TryGetComponent(out NavMeshAgent OldAgent))
+                            {
+                                DestroyImmediate(OldAgent);
+                            }
+
+                            if (Instance.transform.TryGetComponent(out NavMeshObstacle OldObstacle))
+                            {
+                                DestroyImmediate(OldObstacle);
+                            }
+
+                            // remove rtse nav controller (typically in unit_movement)
+                            if (Instance.transform.TryGetComponent(out NavMeshAgentController OldRTSEAgentController))
+                            {
+                                DestroyImmediate(OldRTSEAgentController);
+                            }
+
+                            Instance.AgentsNavController = Instance.AddComponent<AgentsNavController>();
+                            Instance.AgentsNavController.navAgent = Instance.AddComponent<AgentAuthoring>();
+                            Instance.AgentsNavController.agentShape = Instance.AddComponent<AgentCylinderShapeAuthoring>();
+                            Instance.AgentsNavController.agentNavmesh = Instance.AddComponent<AgentNavMeshAuthoring>();
+
+                            if (useSonarAvoidance)
+                            {
+                                Instance.AgentsNavController.agentAvoidance = Instance.AddComponent<AgentAvoidAuthoring>();
+                            }
+                            else
+                            {
+                                Instance.AgentsNavController.agentSeparation = Instance.AddComponent<AgentSeparationAuthoring>();
+                            }
+
+                            Instance.AddComponent<AgentSmartStopAuthoring>();
+                            switch (Instance.UnitType)
+                            {
+                                case RTSEngineNavAgentsIntegration.UnitType.Infantry:
+                                    Instance.AddComponent<InfantryLocomotionAuthoring>();
+                                    break;
+                                case RTSEngineNavAgentsIntegration.UnitType.Wheeled:
+                                    Instance.AddComponent<WheeledLocomotionAuthoring>();
+                                    break;
+                                case RTSEngineNavAgentsIntegration.UnitType.Tracked:
+                                    Instance.AddComponent<TrackedLocomotionAuthoring>();
+                                    break;
+                                case RTSEngineNavAgentsIntegration.UnitType.Air:
+                                    Instance.AddComponent<AircraftLocomotionAuthoring>();
+                                    break;
+                            }
+
+                        }
+                    }
+
+                    EditorGUILayout.EndVertical();
+                    break;
+                case 1:
+                    EditorGUILayout.BeginVertical("box");
+
+
+                    EditorGUILayout.EndVertical();
+                    break;
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
     }
 
 }
