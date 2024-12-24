6593db011b28cfc987fe766961ffd75caab2ee75
anegrete <alan.t.negrete@gmail.com>
2024-12-24 12:29:41 -0600

    Separation avoidance to UntiSetup

--- Scripts/UnitSetup.cs
+++ Scripts/UnitSetup.cs
@@ -1,60 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 using RTSEngine.Entities;
 using RTSEngine.Game;
 
 using ProjectDawn.Navigation.Hybrid;
-using RTSEngine.Movement;
 
-public class UnitSetup : MonoBehaviour, IEntityPreInitializable
+namespace RTSEngineNavAgentsIntegration
 {
-    protected IEntity Entity { get; private set; }
-    protected IUnit Unit { get; private set; }
-
-    public UnitType UnitType = UnitType.None;
-
-    public AgentsNavController AgentsNavController { get; set; }
-    public AgentAuthoring Agent { set; get; }
-    public AgentCylinderShapeAuthoring Shape { set; get; }
-    public AgentNavMeshAuthoring NavMesh { set; get; }
-    public AgentAvoidAuthoring Avoid { set; get; }
-    public AgentSmartStopAuthoring SmartStop { set; get; }
-
-    [HideInInspector]
-    public int TabID = 0;
-    public bool ConfigCompleted = false;
-
-    public void OnEntityPreInit(IGameManager GameMgr, IEntity Entity)
+    public class UnitSetup : MonoBehaviour, IEntityPreInitializable
     {
-        this.Entity = Entity;
-        this.Unit = Entity as IUnit;
+        protected IEntity Entity { get; private set; }
+        protected IUnit Unit { get; private set; }
+
+        public UnitType UnitType = UnitType.None;
+
+        public AvoidanceType AvoidanceType = AvoidanceType.Sonar;
+        public AgentsNavController AgentsNavController { get; set; }
+        public AgentAuthoring Agent { set; get; }
+        public AgentCylinderShapeAuthoring Shape { set; get; }
+        public AgentNavMeshAuthoring NavMesh { set; get; }
+        public AgentAvoidAuthoring Avoid { set; get; }
+        public AgentSmartStopAuthoring SmartStop { set; get; }
+        public AgentSeparationAuthoring Separation { set; get; }
+
+        [HideInInspector]
+        public int TabID = 0;
+        public bool ConfigCompleted = false;
+
+        public void OnEntityPreInit(IGameManager GameMgr, IEntity Entity)
+        {
+            this.Entity = Entity;
+            this.Unit = Entity as IUnit;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+
+        }
+
+        public void Disable()
+        {
+
+        }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public enum UnitType
     {
-        
+        None,
+        Infantry,
+        Wheeled,
+        Tracked,
+        Air
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    public enum AvoidanceType
     {
-        
+        Sonar,
+        Separation
     }
-
-    public void Disable()
-    {
-        
-    }
-}
-
-public enum UnitType
-{
-    None,
-    Infantry,
-    Wheeled,
-    Tracked,
-    Air
 }
